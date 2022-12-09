0455c966907399b016a48c8bbbe66250d43457c3
Piotr Perczak <wojownik102pl@Wp.pl>
2022-12-09 10:03:58 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import Example from './hihi'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    
+   <Example></Example>
+  
+  )
+}
+
+export default App
